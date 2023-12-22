588384cb7c522e6a37699289fc347442883b9ec7
tyler36 <7234392+tyler36@users.noreply.github.com>
2023-12-22 01:01:57 +0000

    Add vite support for DDEV

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
+    server: {
+        host: true,
+        strictPort: true,
+        port: 5173,
+        hmr: {
+            protocol: 'wss',
+            host: `${process.env.DDEV_HOSTNAME}`,
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
